33cbfbb7be54dcf8bb7f8d5c4269a1cf21e033a6
Meme <meme_es@hotmail.com>
2022-03-31 16:21:54 -0600

    Add the create function for items

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -29,4 +29,39 @@ class ItemController extends Controller
 
         return view('items.index', compact('items'));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('items.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data_item = $request->validate([
+            'description' => 'required'
+        ]);
+
+        $data_price = $request->validate([
+            'cost' => ['nullable', 'regex:/^\d+(\.\d{1,})?$/'],
+            'price' => ['required', 'regex:/^\d+(\.\d{1,})?$/']
+        ]);
+
+        $item = Item::create($data_item);
+
+        $data_price['item_id'] = $item->id;
+        Price::create($data_price);
+
+        return redirect('/items');
+    }
 }
---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-10 col-lg-8">
+        <div class="card">
+          <div class="card-header">
+            <span class="fw-bold"><i class="fas fa-map-marked"></i> Agregar Nuevo Artículo o Servicio</span>
+          </div>
+          <div class="card-body">
+            <form action="{{ route('items.store') }}" method="POST">
+              @csrf
+
+              <div class="form-group row mb-3">
+                <label for="description" class="col-md-3 col-form-label text-md-end">{{ __('Descripción') }}<span class="text-danger">*</span></label>
+                <div class="col-md-7">
+                  <input type="text"
+                    name="description"
+                    id="description"
+                    class="form-control @error('description') is-invalid @enderror"
+                    value="{{ old('description') }}"
+                    placeholder="Descripción del artículo o servicio"
+                    required
+                    autofocus>
+
+                  @error('description')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="form-group row mb-3">
+                <label for="cost" class="col-md-3 col-form-label text-md-end">{{ __('Costo') }}</label>
+                <div class="col-md-7">
+                  <input type="text"
+                    name="cost"
+                    id="cost"
+                    class="form-control @error('cost') is-invalid @enderror"
+                    value="{{ old('cost') }}"
+                    placeholder="Costo">
+
+                  @error('cost')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="form-group row mb-3">
+                <label for="price" class="col-md-3 col-form-label text-md-end">{{ __('Precio') }}<span class="text-danger">*</span></label>
+                <div class="col-md-7">
+                  <input type="text"
+                    name="price"
+                    id="price"
+                    class="form-control @error('price') is-invalid @enderror"
+                    value="{{ old('price') }}"
+                    placeholder="Precio"
+                    required>
+
+                  @error('price')
+                    <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-10 text-end">
+                  <a href="{{ route('items.index') }}" class="btn btn-secondary me-1">{{  __('Cancelar') }}</a>
+                  <button type="submit" class="btn btn-primary">{{ __('Agregar') }}</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
