e5a9bd66298b9235da529023cf1f63e715b212bd
Meme <meme_es@hotmail.com>
2022-03-31 16:14:23 -0600

    Add relationships into the item model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['description'];
+
+    public function prices()
+    {
+        return $this->hasMany(Price::class);
+    }
+
+    public function current_price()
+    {
+        return $this->hasOne(Price::class)->latest();
+    }
 }
